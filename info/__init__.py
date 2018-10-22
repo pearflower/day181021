e161883d711f85ad0561661202602416cb047554
木樨 <492501976@qq.com>
2018-10-22 19:52:06 +0800

    windows log commit

--- info/__init__.py
+++ info/__init__.py
@@ -1,3 +1,6 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask
 from info.moduel.index import index_blue
 from flask_session import Session
@@ -6,8 +9,16 @@ from config import Config
 from flask_wtf.csrf import CSRFProtect
 import redis
 
+def set_up_log():
+    logging.basicConfig(level=logging.DEBUG)
+    file_log_handler = RotatingFileHandler('logs/log',maxBytes=104857600,backupCount=10)
+    formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+    file_log_handler.setFormatter(formatter)
+    logging.getLogger().addHandler(file_log_handler)
+
 db = SQLAlchemy()
 def create_app():
+    set_up_log()
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
--- info/moduel/index/views.py
+++ info/moduel/index/views.py
@@ -1,9 +1,17 @@
-from flask import render_template
+from flask import render_template, logging
 
 from . import index_blue
 
 @index_blue.route('/')
 def index():
     # print('come here')
+    # set_log()
     return render_template('index.html')
+
+def set_log():
+    logging.log(logging.DEBUG, "This is a debug log.")
+    logging.log(logging.INFO, "This is a info log.")
+    logging.log(logging.WARNING, "This is a warning log.")
+    logging.log(logging.ERROR, "This is a error log.")
+    logging.log(logging.CRITICAL, "This is a critical log.")
 
