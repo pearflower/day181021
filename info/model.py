61ba253b94b11de813695675c150842d28829660
木樨 <492501976@qq.com>
2018-10-23 15:46:58 +0800

    windows table_one2all commit

--- info/model.py
+++ info/model.py
@@ -11,6 +11,8 @@ class User(db.Model):
 
     # 用户和新闻一对多关联
     user_news = db.relationship('News',backref='user',lazy='dynamic')
+    # 用户和评论一对多关联
+    user_comment = db.relationship('Comment',backref='user',lazy='dynamic')
 
     def __repr__(self):
         return 'user : %s'%self.name
@@ -22,10 +24,11 @@ class News(db.Model):
     __tablename__ = 'news'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(20), nullable=False)
-    content = db.Column(db.String(300))
+    content = db.Column(db.Text)
 
     # 外键
     user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    type_id = db.Column(db.Integer,db.ForeignKey('type.id'))
 
     # 新闻和评论一对多关联
     news_comment = db.relationship('Comment',backref='news',lazy='dynamic')
@@ -40,10 +43,29 @@ class Comment(db.Model):
     '''
     __tablename__ = 'comment'
     id = db.Column(db.Integer,primary_key=True)
-    content = db.Column(db.String(100),nullable=False)
+    content = db.Column(db.Text,nullable=False)
 
     # 外键
+    parent_id = db.Column(db.Integer,db.ForeignKey('comment.id'))
     news_id = db.Column(db.Integer,db.ForeignKey('news.id'))
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+
+    # 自关联
+    parent = db.relationship('Comment',remote_side=[id])
 
     def __repr__(self):
-        return 'comment : %s'%self.content
+        return 'comment : %d'%self.id
+
+class Type(db.Model):
+    '''
+    分类表
+    '''
+    __tablename__ = 'type'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(20),nullable=False)
+
+    # 新闻分类一对多关系
+    type_news = db.relationship('News',backref='type',lazy='dynamic')
+
+    def __repr__(self):
+        return 'type : %s'%self.name
